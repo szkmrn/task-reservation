a08cee4e330399508165aa6c094d23eabf828982
EC2 Default User <ec2-user@ip-172-31-18-151.us-west-1.compute.internal>
2022-07-06 15:32:34 +0000

    userコントローラとaccount/profileアクションの新規作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  
+  
+  
+  before_action :authenticate_user!
+  def account
+    @users= current_user
+  end
+
+  def profile
+    @users= current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/account'
+  get 'users/profile'
   devise_for :users
+  root to: "homes#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/',to:'homes#home'
-  root to: "user#index"
+ 
 end
