5e70ca93ae4d4ac6b1d48ae3513ae3bbf728a37b
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-07-19 20:44:33 +0900

    ルーム情報登録にバリデーションの追加とエラーメッセージ追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,9 @@ class Room < ApplicationRecord
     has_one_attached :picture
     has_one_attached :image
    
+    validates :name,presence:true
+    validates :information,presence:true
+    validates :price,presence:true,numericality:true
+    validates :address,presence:true
+    validates :picture,presence:true
 end
